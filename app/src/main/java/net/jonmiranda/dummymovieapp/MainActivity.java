42ba734ef5c1742c9200bf0fc7ab58d0182336f2
Jon Miranda <miranda.jra@gmail.com>
2015-04-27 22:56:33 -0700

    Added RecyclerView

--- app/src/main/java/net/jonmiranda/dummymovieapp/MainActivity.java
+++ app/src/main/java/net/jonmiranda/dummymovieapp/MainActivity.java
@@ -1,17 +1,83 @@
 package net.jonmiranda.dummymovieapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+
+    public static final String[] MOVIE_NAMES = {
+            "Snow White and the Seven Dwarfs",
+            "Lady and the Tramp",
+            "The Jungle Book",
+            "Cinderella",
+            "Fantasia",
+            "Bambi",
+            "Sleeping Beauty",
+            "Dumbo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // grab reference to the RecyclerView
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+
+        // set up the layout manager
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
+
+        // set up adapter
+        RecyclerView.Adapter<MovieItemViewHolder> adapter = new MovieListAdapter(MOVIE_NAMES);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    static class MovieListAdapter extends RecyclerView.Adapter<MovieItemViewHolder> {
+
+        private final String[] movies;
+
+        public MovieListAdapter(String[] movies) {
+            this.movies = movies;
+        }
+
+        @Override
+        public MovieItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.movie_item_layout, parent, false);
+            return new MovieItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MovieItemViewHolder holder, int position) {
+            holder.movieTitle.setText(movies[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return movies.length;
+        }
+    }
+
+    static class MovieItemViewHolder extends RecyclerView.ViewHolder {
+        protected TextView movieTitle;
+
+        public MovieItemViewHolder(View view) {
+            super(view);
+            movieTitle = (TextView) view.findViewById(R.id.movie_title);
+        }
     }
 
     @Override
