d40a9db6a5d95ce714a0afbb07b77ff5dc4e5cea
Jon Miranda <miranda.jra@gmail.com>
2015-05-24 11:08:57 -0700

    Get movie data from the internet

--- app/src/main/java/net/jonmiranda/dummymovieapp/MainActivity.java
+++ app/src/main/java/net/jonmiranda/dummymovieapp/MainActivity.java
@@ -2,14 +2,11 @@ package net.jonmiranda.dummymovieapp;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,33 +14,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.http.GET;
 
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
 
-    public static final String[] MOVIE_NAMES = {
-            "Snow White and the Seven Dwarfs",
-            "Lady and the Tramp",
-            "The Jungle Book",
-            "Cinderella",
-            "Fantasia",
-            "Bambi",
-            "Sleeping Beauty",
-            "Dumbo"
-    };
+    String API_ENDPOINT = "http://jonmiranda.net/static/dummymovieapp";
 
-    public static final int[] MOVIE_IMAGE_IDS = {
-            R.drawable.snow_white_and_the_seven_dwarfs,
-            R.drawable.lady_and_the_tramp,
-            R.drawable.the_jungle_book,
-            R.drawable.cinderella,
-            R.drawable.fantasia,
-            R.drawable.bambi,
-            R.drawable.sleeping_beauty,
-            R.drawable.dumbo,
-    };
+    public interface MovieProvider {
+        @GET("/movies")
+        void getMovies(Callback<Movie[]> callback);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,33 +47,45 @@ public class MainActivity extends AppCompatActivity {
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(llm);
 
-        // build list of images and palettes
-        final Drawable[] movieImages = new Drawable[MOVIE_IMAGE_IDS.length];
-        final Palette[] palettes = new Palette[MOVIE_IMAGE_IDS.length];
+        // Let's start networking!
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API_ENDPOINT)
+                .build();
 
-        for (int i = 0; i < MOVIE_IMAGE_IDS.length; ++i) {
-            movieImages[i] = getResources().getDrawable(MOVIE_IMAGE_IDS[i]);
-            palettes[i] = Palette.generate(BitmapFactory.decodeResource(getResources(), MOVIE_IMAGE_IDS[i]));
-        }
+        // Create a MovieProvider base off of the RestAdapter we created
+        MovieProvider movieProvider = restAdapter.create(MovieProvider.class);
 
+        // We're going to use a Callback so Retrofit will make the network request
+        // off of the UI thread.
+        movieProvider.getMovies(new Callback<Movie[]>() {
+            @Override
+            public void success(Movie[] movies, Response response) {
+                showMovies(movies);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                String errorMessage = "RetrofitError: " + error.getMessage();
+                Log.e("MainActivity", errorMessage);
+                Toast.makeText(getBaseContext(), errorMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void showMovies(Movie[] movies) {
         // set up adapter
-        RecyclerView.Adapter<MovieItemViewHolder> adapter =
-                new MovieListAdapter(this, MOVIE_NAMES, movieImages, palettes);
+        RecyclerView.Adapter<MovieItemViewHolder> adapter = new MovieListAdapter(this, movies);
         mRecyclerView.setAdapter(adapter);
     }
 
     static class MovieListAdapter extends RecyclerView.Adapter<MovieItemViewHolder> {
 
         private final MainActivity activity;
-        private final String[] titles;
-        private final Drawable[] images;
-        private final Palette[] palettes;
+        private final Movie[] movies;
 
-        public MovieListAdapter(MainActivity activity, String[] titles, Drawable[] images, Palette[] palettes) {
+        public MovieListAdapter(MainActivity activity, Movie[] movies) {
             this.activity = activity;
-            this.titles = titles;
-            this.images = images;
-            this.palettes = palettes;
+            this.movies = movies;
         }
 
         @Override
@@ -96,9 +97,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(final MovieItemViewHolder holder, final int position) {
-            holder.cardView.setBackgroundColor(palettes[position].getVibrantColor(Color.WHITE));
-            holder.movieTitle.setText(titles[position]);
-            holder.movieImage.setImageDrawable(images[position]);
+            holder.cardView.setBackgroundColor(movies[position].color);
+            holder.movieTitle.setText(movies[position].title);
 
             holder.cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
                             .makeSceneTransitionAnimation(activity, holder.movieTitle, "movie_title");
 
                     Intent intent = new Intent(activity, MovieActivity.class);
-                    intent.putExtra(MovieActivity.MOVIE_TITLE_KEY, titles[position]);
+                    intent.putExtra(MovieActivity.MOVIE_TITLE_KEY, movies[position].title);
 
                     // start the new activity
                     activity.startActivity(intent, options.toBundle());
@@ -121,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return titles.length;
+            return movies.length;
         }
     }
 
